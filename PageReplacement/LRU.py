db95ab768e130966ba6b418f6f2a51b2c77d6d32
Kumar Tambe <kumartambe49@gmail.com>
2026-01-01 22:03:23 +0530

    yes

--- PageReplacement/LRU.py
+++ PageReplacement/LRU.py
@@ -1,20 +1,19 @@
-# LRU Page Replacement
+# LRU Page Replacement (Simple)
 pages = [7, 0, 1, 2, 0, 3, 0, 4]
-frames = [None,None,None]
-page_faults = 0
+frames = []
+size = 3
+faults = 0
 
 print("Page -> Frames")
-
 for p in pages:
     if p not in frames:
-        if len(frames) < 3:
-            frames.append(p)
-        else:
-            frames.pop(0)
-            frames.append(p)
-        page_faults += 1
+        if len(frames) == size:
+            frames.pop(0)   # remove least recently used
+        frames.append(p)
+        faults += 1
     else:
         frames.remove(p)
-        frames.append(p)
-    print(p, " -> ", frames)
-print("\nTotal Page Faults (LRU):", page_faults)
+        frames.append(p)    # make it most recently used
+    print(p, "->", frames)
+
+print("Total Page Faults:", faults)
